82499bf5f6a3315eeb358dd7bd3e0d3c3c4a4bbf
Mervat1975 <mervat.mustafa@dcmail.ca>
2022-03-26 17:38:25 -0400

    collectstatic

--- Fish_class/settings.py
+++ Fish_class/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
