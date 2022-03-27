5c379b764ed75617b00a9ca0785ed6939e208ba8
Mervat1975 <mervat.mustafa@dcmail.ca>
2022-03-26 22:22:19 -0400

    debug=false

--- Fish_class/settings.py
+++ Fish_class/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cz(%*$q+*md_4ryg*#^_97m(8-p%)@)#pqm%iye*v@^+(hsl#4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
